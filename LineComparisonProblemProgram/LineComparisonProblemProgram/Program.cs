067b254053a02140cc5e1a1dd9e54f5606008a8c
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-01 09:18:00 +0530

    [Roopa] Using Equal and Compare to method

--- LineComparisonProblemProgram/LineComparisonProblemProgram/Program.cs
+++ LineComparisonProblemProgram/LineComparisonProblemProgram/Program.cs
@@ -43,20 +43,35 @@ namespace LineComparisonProblemProgram
             double length2 = l.calculateLengthOfLine(x3, y3, x4, y4);
             Console.WriteLine("the length of line2 is" + length2);
 
-            if (length1 == length2)
-            {
-                Console.WriteLine("Two Lines are Equal ");
-            }
-            else if (length1 > length2)
-            {
-                Console.WriteLine("Line1 is greater than Line2");
-            }
-            else
-            {
-                Console.WriteLine("Line 1 is less than Line2");
-            }
-        }
+            //using Equals() method to check the equality of two lines of points
 
+            Console.WriteLine("Check The Euality Of two Points");
+            
+            Console.WriteLine("X1 and X2 are equal: " + x1.Equals(x2));
+            Console.WriteLine("Y1 and Y2 are equal: " + y1.Equals(y2));
+            Console.WriteLine("X3 and X4 are eual:  " + x3.Equals(x4));
+            Console.WriteLine("Y3 and Y4 are equal: " + y3.Equals(y4));
+            Console.WriteLine("X1 and X3 are equal: " + x1.Equals(x3));
+            Console.WriteLine("X2 and X4 are equal: " + x2.Equals(x4));
+            Console.WriteLine("Y1 and Y3 are equal: " + y1.Equals(y3));
+            Console.WriteLine("Y2 and Y4 are equal: " + y2.Equals(y4));
+            
+            //using Equals() Method and CompareTo() Method  to Check the Lines are equal or not
+           
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("The two lines are equal");
+            }
+            if (length1.CompareTo(length2) < 0)
+            {
+                Console.WriteLine("The second line is greater than first line");
+            }
+            if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("The first line is greater than second line");
+            }
+            Console.ReadLine();
+        }
 
     }
 }
